f482584afc02e0b278308a5ecfc8f6bd5c84764d
Carl Sargunar <carl@sargunar.com>
2021-09-04 03:09:29 +0100

    wip

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,26 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace ConsoleApp
 {
     class Program
     {
 
+        private const string _todoFile = "todo.json";
         private static List<Todo> _todoList { get; set; }
 
         static void Main(string[] args)
         {
+            _todoList = new List<Todo>();
             Console.WriteLine("Todo App in C#");
 
+            ReadFromFile();
+
+            _todoList.Add(new Todo() { Task = "Tesst", Completed = false });
+            _todoList.Add(new Todo() { Task = "Teadwawd", Completed = true });
+
+
+            WriteToFile();
 
         }
 
-        static void WriteToFile(){
+        static void WriteToFile()
+        {
+            var op = JsonConvert.SerializeObject(_todoList);
+            File.WriteAllText(_todoFile, op);
 
         }
 
         static void ReadFromFile(){
-            
+            Console.WriteLine("Reading from File");
+            if (File.Exists(_todoFile))
+            {
+                Console.WriteLine("File found");
+
+                var fileContent = File.ReadAllText(_todoFile);
+                Console.WriteLine($"Contents: {fileContent}");
+            }
         }
 
         static void ShowList()
