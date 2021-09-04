0214e5988d2b661f160420ceb8c159aedfc9b1c2
Carl Sargunar <carl@sargunar.com>
2021-09-04 02:28:11 +0100

    wip

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+
+        private static List<Todo> _todoList { get; set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Todo App in C#");
+
+
+        }
+
+        static void WriteToFile(){
+
+        }
+
+        static void ReadFromFile(){
+            
+        }
+
+        static void ShowList()
+        {
+            foreach (var todo in _todoList)
+            {
+                Console.WriteLine($"[ ]{todo.Task}");
+                if (string.IsNullOrEmpty(todo.Notes))
+                {
+                    Console.WriteLine($"    {todo.Notes}");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp/ToDo.cs
@@ -0,0 +1,14 @@
+
+using System;
+
+namespace ConsoleApp
+{
+
+    public class Todo
+    {
+        public string Task { get; set; }
+        public string Notes { get; internal set; }
+        public bool Completed { get; set; }
+        public DateTime? DateCompleted { get; set; }
+    }
+}
